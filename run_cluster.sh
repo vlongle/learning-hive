773675dbd63de81e270c2489ce0057e481f7d31c
Long Le <vlongle@neowise.seas.upenn.edu>
2023-03-31 19:57:02 +0000

    cluster training script

---
+++ run_cluster.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+srun --gpus=2\
+ --nodes=1\
+ --cpus-per-gpu=8\
+ --mem-per-cpu=4G\
+ --time=72:00:00\
+ --qos=ee-med\
+ --partition=eaton-compute \
+bash -c "python experiments/experiments.py"
+
+exit 3

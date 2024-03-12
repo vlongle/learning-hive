ae361754caf7e2648b7af1bda094d2642e490895
Long Le <vietlong.lenguyen@gmail.com>
2024-03-12 16:44:21 -0400

    desktop sync ready to run cifar-100 modmod

--- experiments/modmod_experiments.py
+++ experiments/modmod_experiments.py
@@ -26,8 +26,8 @@ def str2bool(v):
 
 parser = argparse.ArgumentParser(
     description='Run experiment with a specified seed.')
-# parser.add_argument('--seed', type=int, default=0,
-#                     help='Seed for the experiment.')
+parser.add_argument('--seed', type=int, default=0,
+                    help='Seed for the experiment.')
 parser.add_argument('--dataset', type=str, default="mnist", choices=[
                     "mnist", "kmnist", "fashionmnist", "cifar100"], help='Dataset for the experiment.')
 parser.add_argument('--sync_base', type=str2bool, default=True)
@@ -49,36 +49,74 @@ if __name__ == "__main__":
     num_epochs = 100
     # num_epochs = 5
 
+    # config = {
+    #     "algo": "modular",
+    #     "agent.batch_size": batch_size,
+    #     "seed": [0, 1, 2, 3, 4, 5, 6, 7],
+    #     # "seed": [0, 1],
+    #     # "seed": 0,
+    #     "parallel": True,
+    #     "num_agents": 8,
+    #     "dataset": "mnist",
+    #     "dataset.num_trains_per_class": 64,
+    #     "dataset.num_vals_per_class": 50,
+    #     "dataset.remap_labels": True,
+    #     "dataset.with_replacement": True,
+    #     "dataset.num_tasks": num_tasks,
+    #     "net": "mlp",
+    #     "net.depth": num_init_tasks,
+    #     'net.no_sparse_basis': args.no_sparse_basis,
+    #     "num_init_tasks": num_init_tasks,
+    #     # "net.dropout": 0.0,
+    #     "net.dropout": 0.5,
+    #     "train.num_epochs": num_epochs,
+    #     "train.component_update_freq": num_epochs,
+    #     "train.init_num_epochs": num_epochs,
+    #     "train.init_component_update_freq": num_epochs,
+    #     "train.save_freq": 10,
+    #     "agent.use_contrastive": [True, False],
+    #     # "agent.use_contrastive": False,
+    #     "agent.memory_size": 32,
+    #     # "dataset": ["mnist", "kmnist", "fashionmnist"],
+    #     "dataset": args.dataset,
+    #     "sharing_strategy": "modmod",
+    #     "sharing_strategy.comm_freq": num_epochs,  # once per task
+    #     "sharing_strategy.opt_with_random": args.opt_with_random,
+    #     "sharing_strategy.sync_base": args.sync_base,
+    #     "sharing_strategy.freeze_candidate_module": args.freeze_candidate_module,
+    #     "sharing_strategy.transfer_decoder": args.transfer_decoder,
+    #     "sharing_strategy.transfer_structure": args.transfer_structure,
+    #     "root_save_dir": f"experiment_results/jorge_setting_lowest_task_id_wins_modmod_test_sync_base_{args.sync_base}_opt_with_random_{args.opt_with_random}_frozen_{args.freeze_candidate_module}_transfer_decoder_{args.transfer_decoder}_transfer_structure_{args.transfer_structure}_no_sparse_basis_{args.no_sparse_basis}",
+    #     "overwrite": False,
+    # }
+
+    # # # === CNN experiments: CIFAR100 ===
+
     config = {
         "algo": "modular",
-        "agent.batch_size": batch_size,
-        "seed": [0, 1, 2, 3, 4, 5, 6, 7],
-        # "seed": [0, 1],
-        # "seed": 0,
+        "seed": args.seed,
         "parallel": True,
-        "num_agents": 8,
-        "dataset": "mnist",
-        "dataset.num_trains_per_class": 64,
-        "dataset.num_vals_per_class": 50,
+        "dataset": "cifar100",
+        "dataset.num_trains_per_class": 256,
+        "dataset.num_vals_per_class": -1,
         "dataset.remap_labels": True,
-        "dataset.with_replacement": True,
-        "dataset.num_tasks": num_tasks,
-        "net": "mlp",
-        "net.depth": num_init_tasks,
-        'net.no_sparse_basis': args.no_sparse_basis,
-        "num_init_tasks": num_init_tasks,
+        "dataset.with_replacement": False,
+        "net": "cnn",
+        "net.depth": 4,
+        "num_init_tasks": 4,
+        "dataset.num_tasks": 20,
         # "net.dropout": 0.0,
         "net.dropout": 0.5,
-        "train.num_epochs": num_epochs,
-        "train.component_update_freq": num_epochs,
         "train.init_num_epochs": num_epochs,
         "train.init_component_update_freq": num_epochs,
-        "train.save_freq": 10,
-        "agent.use_contrastive": [True, False],
-        # "agent.use_contrastive": False,
+        "train.num_epochs": num_epochs,
+        "train.component_update_freq": num_epochs,
         "agent.memory_size": 32,
-        # "dataset": ["mnist", "kmnist", "fashionmnist"],
-        "dataset": args.dataset,
+        "agent.batch_size": 64,
+        "train.save_freq": 10,
+        "agent.use_contrastive": False,
+        'net.no_sparse_basis': args.no_sparse_basis,
+
         "sharing_strategy": "modmod",
         "sharing_strategy.comm_freq": num_epochs,  # once per task
         "sharing_strategy.opt_with_random": args.opt_with_random,
@@ -87,37 +125,7 @@ if __name__ == "__main__":
         "sharing_strategy.transfer_decoder": args.transfer_decoder,
         "sharing_strategy.transfer_structure": args.transfer_structure,
         "root_save_dir": f"experiment_results/jorge_setting_lowest_task_id_wins_modmod_test_sync_base_{args.sync_base}_opt_with_random_{args.opt_with_random}_frozen_{args.freeze_candidate_module}_transfer_decoder_{args.transfer_decoder}_transfer_structure_{args.transfer_structure}_no_sparse_basis_{args.no_sparse_basis}",
-        "overwrite": False,
     }
-
-    # # # === CNN experiments: CIFAR100 ===
-
-    # config = {
-    #     "algo": ["monolithic", "modular"],
-    #     "seed": seed,
-    #     "num_agents": 8,
-    #     # "parallel": True,
-    #     "parallel": False,
-    #     "dataset": "cifar100",
-    #     "dataset.num_trains_per_class": 256,
-    #     "dataset.num_vals_per_class": -1,
-    #     "dataset.remap_labels": True,
-    #     "dataset.with_replacement": False,
-    #     "net": "cnn",
-    #     "net.depth": 4,
-    #     "num_init_tasks": 4,
-    #     "dataset.num_tasks": 20,
-    #     "net.dropout": 0.0,
-    #     "train.init_num_epochs": 300,
-    #     "train.init_component_update_freq": 300,
-    #     "train.num_epochs": 100,
-    #     "train.component_update_freq": 100,
-    #     "agent.memory_size": 32,
-    #     "agent.batch_size": 1024,
-    #     "train.save_freq": 20,
-    #     "agent.use_contrastive": [True, False],
-    #     "root_save_dir": "experiment_results/vanilla_cifar",
-    # }
 
     # print('args', args, type(args.sync_base), type(args.opt_with_random))
     run_experiment(config, strict=False)
--- run_modmod_exp.sh
+++ run_modmod_exp.sh
@@ -1,32 +1,22 @@
 #!/bin/bash
 #SBATCH --output=slurm_outs/modmod/slurm-%j.out
-#SBATCH --gpus=1
+#SBATCH --gpus=2
 #SBATCH --nodes=1
 #SBATCH --cpus-per-gpu=24
-#SBATCH --mem-per-cpu=4G
+#SBATCH --mem-per-cpu=2G
 #SBATCH --time=72:00:00
 #SBATCH --qos=ee-med
 #SBATCH --partition=eaton-compute
-#SBATCH --array=0-23 # Now 24 jobs in total for each combination of settings
+#SBATCH --array=0-7 # Only 8 jobs in total
 
-declare -a datasets=("mnist" "kmnist" "fashionmnist")
-declare -a transfer_decoder=("0" "1")
-declare -a transfer_structure=("0" "1")
-declare -a no_sparse_basis=("0" "1")
+# Fixed settings for this run
+TRANSFER_DECODER="1"
+TRANSFER_STRUCTURE="1"
+NO_SPARSE_BASIS="1"
 
-# Calculate dataset index
-dataset_index=$(($SLURM_ARRAY_TASK_ID / 8)) # 8 combinations of transfer and no_sparse_basis settings per dataset
-# Calculate transfer_decoder index
-transfer_decoder_index=$((($SLURM_ARRAY_TASK_ID / 4) % 2)) # Alternates every 4 jobs
-# Calculate transfer_structure index
-transfer_structure_index=$((($SLURM_ARRAY_TASK_ID / 2) % 2)) # Alternates every 2 jobs
-# Calculate no_sparse_basis index
-no_sparse_basis_index=$(($SLURM_ARRAY_TASK_ID % 2)) # Alternates every job
+# Use SLURM_ARRAY_TASK_ID as the seed for each job
+SEED=$SLURM_ARRAY_TASK_ID
 
-DATASET=${datasets[$dataset_index]}
-TRANSFER_DECODER=${transfer_decoder[$transfer_decoder_index]}
-TRANSFER_STRUCTURE=${transfer_structure[$transfer_structure_index]}
-NO_SPARSE_BASIS=${no_sparse_basis[$no_sparse_basis_index]}
-
-srun bash -c "python experiments/modmod_experiments.py --dataset $DATASET --transfer_decoder $TRANSFER_DECODER --transfer_structure $TRANSFER_STRUCTURE --no_sparse_basis $NO_SPARSE_BASIS"
+# Run the command with the fixed settings and a unique seed for each job
+srun bash -c "python experiments/modmod_experiments.py --transfer_decoder $TRANSFER_DECODER --transfer_structure $TRANSFER_STRUCTURE --no_sparse_basis $NO_SPARSE_BASIS --seed $SEED"
 exit 0

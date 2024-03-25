18ca42b1184d819b6a07aaec91064be0a3e42d3a
vlongle <vietlong.lenguyen@gmail.com>
2024-03-25 21:09:16 +0000

    cluster sync

--- run_cluster_batch.sh
+++ run_cluster_batch.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 #SBATCH --output=slurm_outs/vanilla/%A_%a.out
-#SBATCH --gpus=2
+#SBATCH --gpus=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-gpu=32
-#SBATCH --mem=64G
+#SBATCH --cpus-per-gpu=48
+#SBATCH --mem-per-cpu=2G
 #SBATCH --time=72:00:00
 #SBATCH --qos=ee-med
 #SBATCH --partition=eaton-compute
-#SBATCH --array=0-1 # Two jobs for two algorithms
+#SBATCH --array=0-15 # Two jobs for two algorithms
 
-# Determine algorithm and settings based on SLURM_ARRAY_TASK_ID
-case $SLURM_ARRAY_TASK_ID in
-    0)
-        ALGO="modular"
-        # Add any specific settings for the modular algorithm here
-        ;;
-    1)
-        ALGO="monolithic"
-        # Add any specific settings for the monolithic algorithm here
-        ;;
-esac
+declare -a seeds=("0" "1" "2" "3" "4" "5" "6" "7")
+declare -a algos=("modular" "monolithic")
 
+
+SEED=${seeds[$((SLURM_ARRAY_TASK_ID % 8))]}
+ALGO=${algos[$((SLURM_ARRAY_TASK_ID / 8))]}
 # Use srun to execute the job with the selected algorithm and any additional settings
-srun bash -c "python experiments/experiments.py --algo $ALGO"
+srun bash -c "python experiments/experiments.py --algo $ALGO --sync_base true --seed $SEED"
 
 exit 3

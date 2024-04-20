1f7defe7d99f9a277754e35b3754ffdd70f4807e
Long Le <vietlong.lenguyen@gmail.com>
2024-04-19 20:32:30 -0400

    desktop sync

--- analyze_budget.py
+++ analyze_budget.py
@@ -52,7 +52,6 @@ def analyze_multiple(root_result_dir, num_init_tasks=4, pattern=r".*"):
                             if not re.search(pattern, save_dir):
                                 continue
 
-                            print('save_dir', save_dir)
                             m = Metric(save_dir, num_init_tasks)
                             # extra_algo = f"{result_dir}_{algo}"
                             extra_algo = f"{algo}_{result_dir}"
@@ -79,7 +78,8 @@ def analyze_multiple(root_result_dir, num_init_tasks=4, pattern=r".*"):
 
 if __name__ == "__main__":
     # root_result_dir = "rerun_fashionmnist_recv_results"
-    root_result_dir = "modular_backward_cifar_heuristic_results_small_mem_32"
+    # root_result_dir = "modular_backward_cifar_heuristic_results_small_mem_32/budget"
+    root_result_dir = "new_topology_experiment_results/modmod"
     record = analyze_multiple(root_result_dir)
     print(record.df)
     # get the final accuracy with respect to different algo and dataset

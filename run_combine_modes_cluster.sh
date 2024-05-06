ff506956f182fe572cffcb36f9b100e56119c255
vlongle <vietlong.lenguyen@gmail.com>
2024-05-06 18:32:00 +0000

    monolithic wip cluster sync

--- run_combine_modes_cluster.sh
+++ run_combine_modes_cluster.sh
@@ -5,26 +5,30 @@
 #SBATCH --cpus-per-gpu=48
 #SBATCH --mem-per-cpu=2G
 #SBATCH --time=72:00:00
-#SBATCH --qos=ee-high
-#SBATCH --partition=eaton-compute
-#SBATCH --array=10-31 # Total of 32 tasks (4 combine options * 8 seeds)
+#SBATCH --qos=normal
+#SBATCH --partition=batch
+#SBATCH --array=0-20 # Total of 72 tasks (8 seeds * 3 datasets * 3 combine options)
 
 # Define combine options
-declare -a combine_options=("recv_data+grad_sharing")
+declare -a combine_options=("recv_data+grad_sharing" "grad_sharing" "recv_data")
 declare -a seeds=("0" "1" "2" "3" "4" "5" "6" "7")  # 8 options
 declare -a datasets=("mnist" "kmnist" "fashionmnist")
 
 ALGO="monolithic"
 
-# Calculate combine option and seed indices based on SLURM_ARRAY_TASK_ID
-combine_index=$(($SLURM_ARRAY_TASK_ID / 8))
-seed_index=$(($SLURM_ARRAY_TASK_ID % 8))
+# Calculate indices based on SLURM_ARRAY_TASK_ID
+task_per_option=$((8 * 3)) # 8 seeds * 3 datasets
+combine_index=$(($SLURM_ARRAY_TASK_ID / $task_per_option))
+inner_index=$(($SLURM_ARRAY_TASK_ID % $task_per_option))
+dataset_index=$(($inner_index / 8))
+seed_index=$(($inner_index % 8))
 
-# Get the specific option and seed for the current task
+# Get the specific combine option, dataset, and seed for the current task
 COMBINE=${combine_options[$combine_index]}
+DATASET=${datasets[$dataset_index]}
 SEED=${seeds[$seed_index]}
 
-# Execute the Python script with the specific combine option and seed
+# Execute the Python script with the specific combine option, dataset, and seed
 srun bash -c "python experiments/combine_modes_experiments.py --combine $COMBINE --seed $SEED --dataset $DATASET --algo $ALGO"
 
 exit 0
